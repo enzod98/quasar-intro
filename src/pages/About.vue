6c1e70609b4c348708889e6eb1b19d7f20748827
Enzo <enzod98@hotmail.com>
2021-07-27 21:34:52 -0400

    commit inicial

---
+++ src/pages/About.vue
@@ -0,0 +1,22 @@
+<template>
+    <q-page padding>
+        <h1>Hola about</h1>
+        <div class="q-gutter-xl">
+            <q-btn color="primary">uno</q-btn>
+            <q-btn color="primary">dos</q-btn>
+            <q-btn color="primary">tres</q-btn>
+        </div>
+        <div class="row">
+            <p class="col-12 col-sm-6 col-md-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum eum est deserunt et excepturi, omnis magni, sunt nam aliquam a adipisci laborum dolor, rerum ullam. Repudiandae ut autem tempore similique.</p>
+            <p class="col-12 col-sm-6 col-md-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum eum est deserunt et excepturi, omnis magni, sunt nam aliquam a adipisci laborum dolor, rerum ullam. Repudiandae ut autem tempore similique.</p>
+            <p class="col-12 col-md-6">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum eum est deserunt et excepturi, omnis magni, sunt nam aliquam a adipisci laborum dolor, rerum ullam. Repudiandae ut autem tempore similique.</p>
+        </div>
+        <div class="row bg-primary text-white text-center items-center" style="height: 400px;">
+            <p class="col">Lorem ipsum dolor sit.</p>
+            <p class="col">Lorem ipsum dolor sit.</p>
+            <p class="col">Lorem ipsum dolor sit.</p>
+        </div>
+
+        
+    </q-page>
+</template>
